1cf4f15a3b5aa8c0385a1fb451bb223e7a396049
ChristopherAndradeS <christopherandradebr@gmail.com>
2024-10-07 00:44:57 -0300

    V2.0
    Relógios arrumados

--- html/script.js
+++ html/script.js
@@ -40,36 +40,42 @@ const EventsElementID =
 const EventsData = 
 [
     // Intervalo | Tempo inicial | WeekDay (min)
-    [1440, 420, 0],
+    [(24 * 60), (7 * 60), 0],
+
     [10080, 420, 7],
     // [0, 0, 0],
-    [120, 65, 0],
-    [120, 95, 0],
-    [120, 110, 0],
-    [480, 480, 0],
-    [480, 495, 0],
-    [480, 510, 0],
-    [480, 525, 0],
-    [480, 540, 0],
-    [480, 540 + 15, 0],
-    [480, 540 + 17, 0],
-    [480, 540 + 23, 0],
-    [480, 540 + 31, 0],
-    [480, 540 + 38, 0],
-    [480, 540 + 46, 0],
-    [480, 540 + 50, 0],
-    [480, 540 + 54, 0],
-    [480, 540 + 58, 0],
+    [(2 * 60), (65), 0],
+    [(2 * 60), (60 + 35), 0],
+    [(2 * 60), (60 + 50), 0],
+
+    [(4 * 60), 0, 0],
+    [(4 * 60), 15, 0],
+    [(4 * 60), 30, 0],
+    [(4 * 60), 45, 0],
+    [(4 * 60), 60, 0],
+    [(4 * 60), 60 + 15, 0],
+    [(4 * 60), 60 + 17, 0],
+    [(4 * 60), 60 + 23, 0],
+    [(4 * 60), 60 + 31, 0],
+    [(4 * 60), 60 + 38, 0],
+    [(4 * 60), 60 + 46, 0],
+    [(4 * 60), 60 + 50, 0],
+    [(4 * 60), 60 + 54, 0],
+    [(4 * 60), 60 + 58, 0],
+
     [60, 0, 0],
     [60, 10, 0],
     [60, 15, 0],
     [60, 40, 0],
     [60, 50, 0],
+
     [15, 0, 0],
     [15, 0, 0],
     [15, 0, 0],
     [15, 0, 0],
+
     [60, 40, 0],
+
     [5760, 420, 7],
     [60, 50, 0],
     [60, 0, 0],
@@ -81,18 +87,150 @@ const SkyTimeZone = (-7)
 
 const UserTimeZone = GetUserTimeZone()
 
+function UpdateSystemClock()
+{
+    var today, doc_str;
+
+    today = GetTodaySeconds(UserTimeZone)
+
+    doc_str = `${String(GetTimeHour(today)).padStart(2, '0')}h :`
+    doc_str += `${String(GetTimeMinute(today)).padStart(2, '0')}m :`
+    doc_str += `${String(GetTimeSeconds(today)).padStart(2, '0')}s `
+
+    document.getElementById("clk-local").innerText = doc_str 
+
+    today = GetTodaySeconds(SkyTimeZone)
+
+    doc_str = `${String(GetTimeHour(today)).padStart(2, '0')}h :`
+    doc_str += `${String(GetTimeMinute(today)).padStart(2, '0')}m :`
+    doc_str += `${String(GetTimeSeconds(today)).padStart(2, '0')}s `
+
+    document.getElementById("clk-global").innerText = doc_str 
+
+    var next_event;
+
+    for(var i = 0; i < 32; i++)
+    {
+        next_event = UpdateDocumentEvent(i)
+        UpdateDocumentDiff(i, next_event)
+    }
+}
+
+function UpdateDocumentDiff(clockid, next_event)
+{
+    var element_diff;
+    var day, hour, minute, second;
+    var doc_str = '';
+ 
+    element_diff  = EventsElementID[clockid][1]
+
+    diff = GetEventDiff(EventsData[clockid][0] * 60, next_event, EventsData[clockid][2] * 60)
+
+    day     = GetTimeDay(diff)
+    hour    = GetTimeHour(diff)
+    minute  = GetTimeMinute(diff)
+    second  = GetTimeSeconds(diff)
+
+    if(day > 0)
+        doc_str += `${day} dia(s) `
+
+    doc_str += `${String(hour).padStart(2, '0')}h :`
+    doc_str += `${String(minute).padStart(2, '0')}m :`
+    doc_str += `${String(second).padStart(2, '0')}s `
+
+    document.getElementById(element_diff).innerText = doc_str 
+}
+
+function UpdateDocumentEvent(clockid)
+{
+    var element_event, next_event;
+    var day, hour, minute;
+    var doc_str = '';
+
+    element_event = EventsElementID[clockid][0]
+    
+    next_event = GetNextEventTime(EventsData[clockid][0] * 60, EventsData[clockid][1] * 60)
+
+    day     = GetTimeDay(next_event)
+    hour    = GetTimeHour(next_event)
+    minute  = GetTimeMinute(next_event)
+    
+    if(day == 0) 
+    {   
+        if(GetTodayMinutes(UserTimeZone) < (next_event / 60)) 
+            doc_str = "Hoje\n"
+        else
+            doc_str = "Amanhã\n" 
+    }
+
+    else if(day == 1) 
+        doc_str = "Amanhã\n"
+
+    else
+    {
+        var date = new Date()
+        date.setDate(date.getDate() + (day - 1))
+        doc_str = `${StrWeek[date.getDay()]}\n`
+    }
+
+    doc_str += `${String(hour).padStart(2, '0')}h `
+    doc_str += `${String(minute).padStart(2, '0')}m`
+
+    document.getElementById(element_event).innerText = doc_str
+
+    return next_event
+}
+
+function GetEventDiff(e_interval, next_event, e_weekday)
+{
+    var seconds = GetTodaySeconds(UserTimeZone)
+
+    if(e_weekday)
+        seconds += (GetTodayWeekDay()) * 86400
+
+    return (e_interval - (seconds - next_event)) % e_interval
+}
+
+function GetNextEventTime(e_interval, initial_time)
+{    
+    var today_sec   = GetTodaySeconds(0) 
+
+    var next_event = initial_time + e_interval;
+
+    for(var i = initial_time; i <= today_sec; i += e_interval)
+        next_event = i + e_interval
+    
+    next_event = next_event + (UserTimeZone * 3600)
+    next_event = next_event > 0 ? next_event : 86400 + next_event
+
+    return next_event
+}
+
 function GetUserTimeZone()
 {
     const date = new Date(Date.now())
     return Math.floor(date.getTimezoneOffset() * (-1) / 60)
 }
 
-function GetTodaySeconds() {
-    return (Math.floor(new Date().getTime() / 1000) % 86400)}
+function GetTodaySeconds(gmt) 
+{
+    var date = GetUTCDate()
+    var seconds = (date.getHours() + gmt) * 3600 + date.getMinutes() * 60 + date.getSeconds()
+    seconds = seconds > 0 ? seconds : 86400 + seconds
+    return seconds
+}
 
-function GetTodayMinutes() {
-    return Math.floor(GetTodaySeconds() / 60)}
+function GetTodayHour(gmt) 
+{
+    var date = GetUTCDate()
+    return (date.getHours() + gmt)
+}
 
+function GetTodayMinutes(gmt) 
+{
+    var date = GetUTCDate()
+    return (date.getHours() + gmt) * 60 + date.getMinutes()
+}
 function GetTodayWeekDay() {
     return new Date().getDay() }
 
@@ -113,140 +251,6 @@ function GetUTCDate()
     var date = new Date(Date.now())
     date.setHours(date.getHours() - GetUserTimeZone())
     return date
-}
-
-function UpdateSystemClock()
-{
-    var next_event_time = 0;
-    var date = GetUTCDate()
-
-    var day, hour, min, sec;
-
-    date.setHours(date.getHours() + UserTimeZone)
-
-    hour = date.getHours()
-    min  = date.getMinutes()
-    sec  = date.getSeconds() 
-
-    hour = String(hour).padStart(2, '0')
-    min  = String(min).padStart(2, '0')
-    sec  = String(sec).padStart(2, '0')
-
-    document.getElementById("clk-local").innerText = `${hour}:${min}:${sec}`
-
-    date.setHours(date.getHours() - UserTimeZone + SkyTimeZone)
-
-    hour = date.getHours()
-    hour = String(hour).padStart(2, '0')
-
-    document.getElementById("clk-global").innerText = `${hour}:${min}:${sec}`
-
-    date.setHours(date.getHours() - SkyTimeZone)
-
-    for(var i = 0; i < 32; i++)
-    {
-        var element_event = EventsElementID[i][0]
-        var element_diff  = EventsElementID[i][1]
-
-        var e_interval    = (EventsData[i][0]) * 60
-        var e_init_time   = (EventsData[i][1]) * 60
-        var e_weekday    = EventsData[i][2]
-
-        var next_event_time = GetNextEventTime(e_interval, e_init_time)
-
-        hour = GetTimeHour(next_event_time) + UserTimeZone
-        min  = GetTimeMinute(next_event_time)
-    
-        hour = String(hour).padStart(2, '0')
-        min  = String(min).padStart(2, '0')
-    
-        var diff = GetEventDiff(e_interval, e_weekday, next_event_time)
-
-        if(e_weekday)
-        {
-            tmp_date = new Date(Date.now() + (diff * 1000))
-
-            day = tmp_date.getDay()
-            day = String("Próximo(a) " + StrWeek[day] + "\n")
-
-            if(GetTodayWeekDay() - day == 1)
-                day = "Amanhã\n"
-            if(GetTodayWeekDay() - day == 0)
-                day = "Hoje\n"
-
-            document.getElementById(element_event).innerText = `${day} ${hour}h ${min}m`
-        }
-
-        else
-        {
-            day = GetTimeDay(diff)
-
-            if(day == 1)
-                document.getElementById(element_event).innerText = `Amanhã\n${hour}h ${min}m`
-
-            else if(day == 0)
-                document.getElementById(element_event).innerText = `Hoje\n${hour}h ${min}m`
-        }
-
-        day = GetTimeDay(diff)
-        hour = GetTimeHour(diff) 
-        min  = GetTimeMinute(diff)
-        sec  = GetTimeSeconds(diff)
-
-        day = String(day).padStart(2, '0')
-        hour = String(hour).padStart(2, '0')
-        min  = String(min).padStart(2, '0')
-        sec  = String(sec).padStart(2, '0')
-
-        if(day != 0)
-            document.getElementById(element_diff).innerText  = `${day} dia(s) ${hour}:${min}:${sec}`
-        else
-            document.getElementById(element_diff).innerText  = `${hour}:${min}:${sec}`
-    
-        switch(i)
-        {
-            case 29:
-            {
-                var str;
-                
-                var weekday = date.getDay()
-            
-                str = 0 <= weekday <= 3 ? "Clima Atual: Chuvoso" : "Clima Atual: Ensolarado"
-                document.getElementById("clk-treehouse-weather-state").innerText = str
-                
-                str = "Mudanças de clima:\n" + document.getElementById(EventsElementID[i][0]).innerText
-                document.getElementById(EventsElementID[i][0]).innerText = str
-            }
-        }
-    }
-}
-
-function GetEventDiff(e_interval, e_weekday, next_event_time)
-{
-    var diff, seconds;
-
-    seconds = GetTodaySeconds()
-
-    if(e_weekday)
-    {
-        seconds += (GetTodayWeekDay()) * 86400
-    }
-
-    diff = (e_interval - (seconds - next_event_time)) % e_interval
-
-    return diff
-}
-
-function GetNextEventTime(e_interval, e_init_time)
-{    
-    today_sec = GetTodaySeconds() 
-    
-    var i = e_init_time
-
-    while(i <= today_sec)
-        i += e_interval
-    
-    return i
 }
 
 document.getElementById("had-local").innerText = `Horário Atual (GMT ${UserTimeZone}:00)`
